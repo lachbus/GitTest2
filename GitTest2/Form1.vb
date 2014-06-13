be1cfdb0a9ccdec97c347de2ae4932a3f55d5911
Stephan <stephan.karathanos@efi.com>
2014-06-12 21:17:36 -0500

    code added in naperville

--- GitTest2/Form1.vb
+++ GitTest2/Form1.vb
@@ -1,6 +1,8 @@
 ﻿Public Class FormGitTest2
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        ' out some test code here for second change in GIT
+        ' put some test code here for second change in GIT
+
+        ' some more test code made on my naperville machine
     End Sub
 End Class
