32230a6018921398756869ab9389e4b04cb293f4
Stephan <stephan.karathanos@efi.com>
2014-06-12 21:03:55 -0500

    Added some code to Button 2

--- GitTest2/Form1.vb
+++ GitTest2/Form1.vb
@@ -1,3 +1,6 @@
 ﻿Public Class FormGitTest2
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' out some test code here for second change in GIT
+    End Sub
 End Class
